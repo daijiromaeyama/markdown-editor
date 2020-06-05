6c84500ebcbca095e870b820aac8fc0bfaac387e
daijiro.maeyama <daijiro.maeyama13@gmail.com>
2020-06-05 19:13:21 +0900

    fix _setValue

--- src/hooks/use_state_with_storage.ts
+++ src/hooks/use_state_with_storage.ts
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 
 export const useStateWithStorage = (init: string, key: string): [string, (s: string) => void] => {
-  const [value, _setValue] = useState<string>(localStorage.getItem(key) || init)
+  const [value, setValue] = useState<string>(localStorage.getItem(key) || init)
 
-  const setValue = (nextValue: string): void => {
-    _setValue(nextValue)
+  const setValueWithStorage = (nextValue: string): void => {
+    setValue(nextValue)
     localStorage.setItem(key, nextValue)
   }
 
-  return [value, setValue]
+  return [value, setValueWithStorage]
 }
